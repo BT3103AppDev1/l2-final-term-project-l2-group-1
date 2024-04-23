95f8cc6bb110d4e1ac756d9ac09995ad221f5935
Ming Xuan <131254017+sickteas@users.noreply.github.com>
2024-04-23 23:19:21 +0800

    Update Home.vue
    Added current month expenditure component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,94 +1,90 @@
 <template>
-  <div id ="background-container">
+  <div id="background-container">
     <div class="header-container">
-                <h1 class="header-text">FinWise</h1>
-                <img class="logo" src="@/assets/Logo.jpg">
+      <h1 class="header-text">FinWise</h1>
+      <img class="logo" src="@/assets/Logo.jpg" />
     </div>
-    <div style="text-align:center;" v-if="user">
+    <div style="text-align: center" v-if="user">
       <div class="Parallax"></div>
-        <div class="NavBarComp"><NavBar/></div>
-        <div class="WelcomeComp"><WelcomeCpp/></div>
-        <div class="AddCoinComp"><AddCoin  @added="change" /></div>
-        <div  class="TotalComp"><TotalAmount/></div>
-      <!-- <ProfitDisplay :key="refreshComp" /> -->
-    
-     <LogOut/>
+      <div class="NavBarComp"><NavBar /></div>
+      <div class="WelcomeComp"><WelcomeCpp /></div>
+      <div class="AddCoinComp"><AddCoin @added="change" /></div>
+      <div class="CurrMonthExp"><CurrMonthExp /></div>
+
+      <LogOut />
     </div>
   </div>
 </template>
 
 <script>
-
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import WelcomeCpp from '@/components/WelcomeCpp.vue'
-import AddCoin from '@/components/AddCoin.vue'
-import ProfitDisplay from '@/components/ProfitDisplay.vue'
-import NavBar from '@/components/NavBar.vue'
-import LogOut from '@/components/LogOut.vue'
-import TotalAmount from '@/components/TotalAmount.vue'
+import WelcomeCpp from "@/components/WelcomeCpp.vue";
+import AddCoin from "@/components/AddCoin.vue";
+import ProfitDisplay from "@/components/ProfitDisplay.vue";
+import NavBar from "@/components/NavBar.vue";
+import LogOut from "@/components/LogOut.vue";
+import CurrMonthExp from "../components/CurrMonthExp.vue";
 
 export default {
-  name: 'Home',
-  components:{
+  name: "Home",
+  components: {
     WelcomeCpp,
     AddCoin,
     ProfitDisplay,
     NavBar,
     LogOut,
-    TotalAmount
+    CurrMonthExp,
   },
 
-  data(){
-    return{
-      user:false,
-      refreshComp:0
-      }
-    },
+  data() {
+    return {
+      user: false,
+      refreshComp: 0,
+    };
+  },
 
   mounted() {
     const auth = getAuth();
-    onAuthStateChanged(auth, user => {
+    onAuthStateChanged(auth, (user) => {
       this.user = user;
-      })
+    });
   },
-  
-  methods:{
 
-  change(){
-     this.refreshComp += 1
-    }
-  }
-}
-
+  methods: {
+    change() {
+      this.refreshComp += 1;
+    },
+  },
+};
 </script>
 
 <style scoped>
-#background-container { 
+#background-container {
   background-color: rgb(255, 255, 255);
-} 
+}
 
 .header-container {
-    display: flex; 
-    align-items: center; 
-    justify-content: flex-start; 
-    grid-column: 1 / -1; 
-    grid-row: 1;
-    width: 100%;
-    text-align: left;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  grid-column: 1 / -1;
+  grid-row: 1;
+  width: 100%;
+  text-align: left;
 }
 
 .header-text {
-    display: inline-block; 
-    font-size: 20px;
-    color: #4116b7; 
-    margin-top: 20px;
+  display: inline-block;
+  font-size: 20px;
+  color: #4116b7;
+  margin-top: 20px;
 }
 
 .logo {
-    height: 35px; 
-    width: auto;
-    margin-top: 10px;
-    margin-left: 3px; 
+  height: 35px;
+  width: auto;
+  margin-top: 10px;
+  margin-left: 3px;
 }
 
 /* .Parallax {
